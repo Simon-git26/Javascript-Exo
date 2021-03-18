7aedaa50b753c778370f3bff517296ebeb40a9e8
Simon Balleux <simoncestmoi@hotmail.fr>
2021-03-18 12:35:59 +0100

    ajout fichier

--- app2.js
+++ app2.js
@@ -1,2 +1,11 @@
-https://api.jikan.moe/v3/search/anime?q=
+/* https://api.jikan.moe/v3/search/anime?q= */
 
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        var response = JSON.parse(this.responseText);
+    }
+};
+request.open("GET", "https://api.jikan.moe/v3/search/anime?q=bleach");
+request.send();
