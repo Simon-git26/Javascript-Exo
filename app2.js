f32bee2c9087192d0d160574341aade2df55a6b9
Simon Balleux <simoncestmoi@hotmail.fr>
2021-03-19 12:10:34 +0100

    Exo de streaming manga

--- app2.js
+++ app2.js
@@ -1,11 +1,38 @@
-/* https://api.jikan.moe/v3/search/anime?q= */
 
-
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
+const fetchUse = async function (search) {
+    let response = await fetch('https://api.jikan.moe/v3/search/anime?q=' + search);
+    if (response.ok) {
+        let data = await response.json();
+        return data;
+    } else {
+        console.error('retour du serveur : ', response.status);
     }
-};
-request.open("GET", "https://api.jikan.moe/v3/search/anime?q=bleach");
-request.send();
+}
+
+let searchValue;
+const input = document.querySelector('input');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  searchValue = e.target.value;
+}
+
+
+async function onFormSubmit() {
+    const data = await fetchUse(searchValue);
+    console.log(data);
+
+    
+    const container = document.getElementById('container');
+    let template = document.querySelector("#productrow");
+
+    for (const result of data.results) {
+        var tbody = document.querySelector("tbody");
+        var clone = document.importNode(template.content, true);
+        var td = clone.querySelectorAll("td");
+        td[0].textContent = data.results;
+
+        container.appendChild(clone);
+    }
+}
